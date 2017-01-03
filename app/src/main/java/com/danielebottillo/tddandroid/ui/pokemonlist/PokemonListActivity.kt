20b787eacf8ae0190d0c924f48f5b8f19440bb12
Daniele Bottillo <dbottillo@gmail.com>
2017-01-03 13:51:02 +0100

    first commit

---
+++ app/src/main/java/com/danielebottillo/tddandroid/ui/pokemonlist/PokemonListActivity.kt
@@ -0,0 +1,13 @@
+package com.danielebottillo.tddandroid.ui.pokemonlist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.danielebottillo.tddandroid.R
+
+class PokemonListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
